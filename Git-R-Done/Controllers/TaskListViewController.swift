295081e3ed4afc8fd9fcb8e4796389bfd5031ccd
Brian Gass <briangass3@gmail.com>
2019-09-11 11:15:39 -0400

    Persist data using Core Data

--- Git-R-Done/Controllers/TaskListViewController.swift
+++ Git-R-Done/Controllers/TaskListViewController.swift
@@ -7,22 +7,28 @@
 //
 
 import UIKit
+import CoreData
 
 class TaskListViewController: UITableViewController {
     
     var taskListArray = [Item]()
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    var selectedCategory: Category? {
+        
+        didSet {
+            loadItems()
+        }
+        
+    }
 
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(dataFilePath)
-        
-        loadItems()
-        
     }
     
-    //MARK - TableView Datasource Methods
+    //MARK: - TableView Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return taskListArray.count
@@ -41,11 +47,13 @@ class TaskListViewController: UITableViewController {
         return cell
     }
     
-    //MARK - TableView Delegate Methods
+    //MARK: - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         taskListArray[indexPath.row].done = !taskListArray[indexPath.row].done
+//        context.delete(taskListArray[indexPath.row])
+//        taskListArray.remove(at: indexPath.row)
         
         saveItems()
         
@@ -53,7 +61,7 @@ class TaskListViewController: UITableViewController {
         //let selectedCell = taskListArray[indexPath.row]
     }
     
-    //MARK - Add new items
+    //MARK: - Add new items
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
@@ -64,8 +72,10 @@ class TaskListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add item", style: .default) { (action) in
             //what will happen once the user clicks add item button on alert
             
-            let newItem = Item()
+            let newItem = Item(context: self.context)
             newItem.title = textField.text!
+            newItem.done = false
+            newItem.parentCategory = self.selectedCategory
             
             self.taskListArray.append(newItem)
             
@@ -84,38 +94,68 @@ class TaskListViewController: UITableViewController {
         
     }
     
-    // MARK - Model manipulation methods
+    // MARK: - Model manipulation methods
     
     func saveItems() {
         
-        let encoder = PropertyListEncoder()
-        
         do {
-            
-            let datar = try encoder.encode(taskListArray)
-            try datar.write(to: dataFilePath!)
-            
+            try context.save()
         } catch{
-            print("Error encoding item array, \(error)")
+            print("Error saving context \(error)")
         }
         self.tableView.reloadData()
         
     }
     
-    func loadItems() {
-        if let data = try? Data.init(contentsOf: dataFilePath!){
-            
-            let decoder = PropertyListDecoder()
-            do {
-            taskListArray = try decoder.decode([Item].self, from: data)
-            } catch {
-                print("Error decoding item array, \(error)")
-            }
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+        
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        
+        if let additionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
+        } else {
+            request.predicate = categoryPredicate
         }
+        
+        do {
+            
+        taskListArray = try context.fetch(request)
+            print(request)
+
+        } catch {
+            
+            print("Error fetching data from context \(error)")
+            
+        }
+        self.tableView.reloadData()
     }
     
 }
 
+// MARK: - Search Bar Methods
+
+extension TaskListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request, predicate: predicate)
+
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
+}
 
 
 
